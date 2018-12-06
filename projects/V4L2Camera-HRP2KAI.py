1c782210390cc66fa00e35161161712f40eaff15
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2018-12-06 14:24:59 +0900

    Cleaning up python script.

--- projects/V4L2Camera-HRP2KAI.py
+++ projects/V4L2Camera-HRP2KAI.py
@@ -1,38 +1,38 @@
-import sys,os,datetime
-
-import rtm
+import sys,os,datetime,socket,rtm
 from rtm import *
-
-import socket
-
-rtm.nsport = 2809
-
 import cnoid.Corba
 
-rtm.orb = cnoid.Corba.getORB()
-if rtm.nshost == None:
-   rtm.nshost = socket.gethostname()
-nsloc = "corbaloc:iiop:%s:%d/NameService" % (rtm.nshost, rtm.nsport)
-print nsloc
-rtm.rootnc = rtm.orb.string_to_object(nsloc)._narrow(CosNaming.NamingContext)
-sys.argv = []
+def setNameserver(nshost, nsport=2809):
+   if nshost == None:
+      rtm.nshost = socket.gethostname()
+   else:
+      rtm.nshost = nshost
+   rtm.nsport = nsport
+   nsloc = "corbaloc:iiop:%s:%d/NameService" % (rtm.nshost, rtm.nsport)
+   print nsloc
+   rtm.rootnc = rtm.orb.string_to_object(nsloc)._narrow(CosNaming.NamingContext)
+      
+try:
+   rtm.orb = cnoid.Corba.getORB()
 
-viewer = rtm.findRTC("ImageViewer0")
-cpanel = rtm.findRTC("ControlPanel0")
+   setNameserver(None)
+   viewer = rtm.findRTC("ImageViewer0")
+   if viewer == None:
+      raise Exception("Failed to find ImageViewer0")
+   cpanel = rtm.findRTC("ControlPanel0")
+   if cpanel == None:
+      raise Exception("Failed to find ControlPanel0")
 
-rtm.nshost = "hrp2001v"
-nsloc = "corbaloc:iiop:%s:%d/NameService" % (rtm.nshost, rtm.nsport)
-print nsloc
-rtm.rootnc = rtm.orb.string_to_object(nsloc)._narrow(CosNaming.NamingContext)
+   setNameserver("hrp2001v")
+   camera = rtm.findRTC("V4L2Camera0")
+   if camera == None:
+      raise Exception("Failed to find V4L2Camera0")
 
-camera = rtm.findRTC("V4L2Camera0")
-
-connectPorts(cpanel.port("Command"), camera.port("Command"))
-connectPorts(camera.port("TimedCameraImage"), viewer.port("images"))
-
-cpanel.start()
-camera.start()
-viewer.start()
-
-
-    
+   connectPorts(cpanel.port("Command"), camera.port("Command"))
+   connectPorts(camera.port("TimedCameraImage"), viewer.port("images"))
+   
+   cpanel.start()
+   camera.start()
+   viewer.start()
+except Exception as err:
+   print(err)
